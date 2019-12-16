05d345272cdf5712a6c6792cafde0b6713bbb174
Zhihang-Liu <enterindex@gmail.com>
2019-12-16 21:05:33 +0800

    >_> Ast必须与编辑器的Ast严格相等。因为Ast索引就是唯一的位址了，通过语法糖变换过后位置信息就被扔了。。。

--- Pigeon/Ast.fs
+++ Pigeon/Ast.fs
@@ -1,14 +1,22 @@
 ﻿module Ast
 
-//type TypeExpr =
-//    | TypeApply of string * TypeExpr list
-//    | UnionType of string * TypeExpr list
-//    | TupleType of TypeExpr list
-//    | Record    of TypeExpr list
-//    | Callable  of TypeExpr list
-//    | TypeName  of string
+type Id = int
+type Level = int
 
-type Pattern = unit
+type Name = string
+
+type TypeExpr =
+    | TypeApply of Name * TypeExpr list
+    | UnionType of Name * TypeExpr list
+    | TupleType of TypeExpr list
+    | Record    of TypeExpr list
+    | Callable  of TypeExpr list
+    | TypeName  of Name
+    | TypeVar   of TVar
+and TVar =
+    | Ref   of Name
+    | Link  of TypeExpr
+    | Id    of Id
 
 type LiteralValue =
     | Bool      of bool
@@ -17,53 +25,29 @@ type LiteralValue =
     | Uint      of uint64
     | Float     of double
     | String    of string
-    | Symbol    of string (*你的恶趣味*)
+    //| Symbol    of string (*你的恶趣味*)
     | RowEmpt   of string
-type Name = string
-type Expr =
+
+// Deconstruction
+type Pattern =
     | Variable of Name
-    | FuncCall of Expr* Expr list
-    | Func of Name list * Expr
-    | LetIn of Name * Expr * Expr
-    | Lit of LiteralValue
-    | RowGet of Expr * Name
-    | RowExtend of Name * Expr * Expr
-    // FIXME:
-    // 这些恶趣味都是啥?
-    // 能不能是语法糖?
-    //| Match         of (Pattern * Expr option * Expr) list
-    //| Cond          of (Expr * Expr) list
-    //| Loop          of                  Expr list
-    //| While         of           Expr * Expr list
-    //| For           of Pattern * Expr * Expr list
+    | MatchVar of Name
+    | Const of LiteralValue
+    | Tuple of Pattern list
+    | Union of Name * Pattern
+    | Ignore
+
+type Expr =
+    | Variable      of Name
+    | FuncCall      of Expr * Expr list
+    | Func          of Name * Pattern list * Expr
+    | Lambda        of Pattern list * Expr
+    | LetIn         of Pattern * Expr * Expr
     //| Let           of Pattern * Expr
-    | Literal       of LiteralValue
-
-// FIXME: Env给你吃了.jpg
-// type FuncDef = string * string list * Expr list
-
-// 用意不明
-//type TopLevel =
-//    | Comment       of string
-//    | TypeAssert    of TypeAssert
-//    | Funcdef       of FuncDef
-//    | Let           of Pattern * Expr
-//    | Do            of Expr list
-
-type Id = int
-type Level = int
-
-type TypeExpr =
-    | TConst of Name (*就像Int Bool这种*)
-    | TApp of TypeExpr * TypeExpr list
-    | TArrow of TypeExpr list// * TypeExpr
-    | TVar of TVar ref
-    | TRow of TypeExpr (*只使用RowEmpt和RowExtend*)
-    | TRowEmpt
-    | TRowExtend of Name * TypeExpr * TypeExpr
-and TVar =
-    | Unbound of Id * Level
-    | Link of TypeExpr (*TypeExpr 垃圾F#*)
-    | Generic of Id
-
-(*请自己写tostr*)    
+    | Lit           of LiteralValue
+    | RowGet        of Expr * Name
+    | Match         of (Pattern * Expr option * Expr) list
+    | Cond          of (Expr * Expr) list
+    | Loop          of                  Expr list
+    | While         of           Expr * Expr list
+    | For           of Pattern * Expr * Expr list
--- PigeonText/Program.fs
+++ PigeonText/Program.fs
@@ -5,7 +5,10 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
-        Func (["a"], Variable "a")
-        |> infer (Map []) 0
-        |> printfn "type: %A"
+        (Func (["a"], Variable "a")
+        |> infer (Map []) 0).ToString ()
+        |> printfn "type: %s"
+        (Func (["a"], Lit (Int 1L))
+        |> infer (Map []) 0).ToString ()
+        |> printfn "type: %s"
         0
