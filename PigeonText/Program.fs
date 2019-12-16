8e460ab6f738f220221cac0d7f8ce4027a0f4347
Zhihang-Liu <enterindex@gmail.com>
2019-12-16 21:11:02 +0800

    删了之前的测试

--- PigeonText/Program.fs
+++ PigeonText/Program.fs
@@ -5,10 +5,10 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
-        (Func (["a"], Variable "a")
+        (*(Func (["a"], Variable "a")
         |> infer (Map []) 0).ToString ()
         |> printfn "type: %s"
         (Func (["a"], Lit (Int 1L))
         |> infer (Map []) 0).ToString ()
-        |> printfn "type: %s"
+        |> printfn "type: %s"*)
         0
